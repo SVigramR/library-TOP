c4ad0dd4ddc7c460ff4b68a11895ee363bdc6adc
SVigramR <87016515+SVigramR@users.noreply.github.com>
2023-01-10 11:41:16 +0530

    Added some form variables in javascript

---
+++ script.js
@@ -0,0 +1,5 @@
+let titleInput = document.getElementById("title").value;
+let authorInput = document.getElementById("author").value;
+let pagesInput = document.getElementById("pages").value;
+let mark = document.getElementById("mark");
+let submitBtn = document.querySelector('.form-submit');
