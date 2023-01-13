f6c8820437f7bb8b8627db813bf59d6df38cefee
SVigramR <87016515+SVigramR@users.noreply.github.com>
2023-01-13 08:24:49 +0530

    Adding event listener to title input

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// popup section
 const openFormButton = document.querySelectorAll('[data-modal-target]');
 const closeFormButton = document.querySelectorAll('[data-close-button]');
 const popupBackground = document.getElementById('popup-background');
@@ -34,3 +35,17 @@ function closePopup(popup) {
     popup.classList.remove('active');
     popupBackground.classList.remove('active');
 }
+
+// form input
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const pages = document.querySelector('#pages');
+const checkBoxLabel = document.querySelector('#readCheckBox');
+const mark = document.querySelector('#mark');
+const errTitle = document.querySelector('#err-title');
+
+title.addEventListener('input', () => {
+    if (title.value === '') {
+        errTitle.textContent = "Please type in your title of the book."
+    }
+})
